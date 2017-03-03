9b3a11a910a67b7104aaa10ca07ed3f2241ae0e3
Yohann Bosco <yohann.bosco@gmail.com>
2017-03-03 16:07:58 +0100

    send first tweet

--- bot.js
+++ bot.js
@@ -3,4 +3,12 @@ var Twit = require('twit');
 
 var Bot = new Twit(config);
 
-console.log('The bot is running...');
+var message = process.argv[2];
+
+console.log(message);
+
+Bot.post('statuses/update', { status: message }, function(err, data, response) {
+  if(err) {
+    console.log("There was a problem tweeting the message.", err);
+  }
+});
