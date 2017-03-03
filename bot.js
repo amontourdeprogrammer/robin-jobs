0092d87fde43662b1e2fbcbd28e69b566753b863
Simon <sims38930@gmail.com>
2017-03-03 17:12:32 +0100

    get website json with job offers

--- bot.js
+++ bot.js
@@ -1,12 +1,14 @@
 var config = require("./config.js");
 var Twit = require('twit');
+var got = require('got');
 
-var Bot = new Twit(config);
+got("https://emploi.alsacreations.com/offres.json")
+  .then(function(response){ console.log(response.body)});
 
 var message = process.argv[2];
-
 console.log(message);
 
+var Bot = new Twit(config);
 Bot.post('statuses/update', { status: message }, function(err, data, response) {
   if(err) {
     console.log("There was a problem tweeting the message.", err);
